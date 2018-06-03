828e94b7ad822e0bd18bea68e6fb48d3a6370736
while (true) <1748893247@qq.com>
2018-06-03 23:51:04 +0800

    添加了类型转换器来映射枚举类,经测试,确实是可行的,要看看类型转换器的原理了

---
+++ src/main/java/com/zxc/movie/dao/MovieTypeMapper.java
@@ -0,0 +1,8 @@
+package com.zxc.movie.dao;
+
+/**
+ * Created by 星驰太帅了 on 2018/06/03.
+ * 操作电影类型的数据库层
+ */
+public class MovieTypeMapper {
+}
--- src/test/java/com/zxc/movie/impl/movie/MovieServiceTest.java
+++ src/test/java/com/zxc/movie/impl/movie/MovieServiceTest.java
@@ -1,11 +1,15 @@
 package com.zxc.movie.impl.movie;
 
 import com.zxc.movie.beans.Movie;
+import com.zxc.movie.common.MovieStatus;
 import com.zxc.movie.service.MovieService;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * Created by 星驰太帅了 on 2018/05/27.
@@ -16,19 +20,22 @@ public class MovieServiceTest extends BaseTest {
     @Autowired
     private MovieService movieService;
 
-
     @Test
-    public void testGetById() {
-        System.out.println(movieService.getById(21));
-        Assert.assertNotNull(movieService.getById(21));
+    public void testInsert() {
+        Movie movie = getMovie(10);
+        int count = movieService.add(movie);
+        System.out.println(count);
     }
 
     @Test
-    public void testInsert() throws Exception{
-        Movie movie = new Movie();
-        movie.setMovieName("i am good");
-        movie.setMovieUrl("www.zxc.com");
-        movie.setStatus(2);
+    public void testGetById() {
+        System.out.println(movieService.getById(5));
+     //   Assert.assertNotNull(movieService.getById(1));
+    }
+
+    @Test
+    public void testInsertAndGet() throws Exception{
+        Movie movie = getMovie(20);
         int count = movieService.add(movie);
         System.out.println(count);
         System.out.println(movie.getId());
@@ -37,5 +44,18 @@ public class MovieServiceTest extends BaseTest {
         //获取刚刚插入的数据,一定可以获取，为了避免网路问题,休眠一下再查询
         Thread.sleep(2000l);
         Assert.assertNotNull(movieService.getById(movie.getId()));
+    }
+
+    private Movie getMovie (int count) {
+        Movie movie = new Movie();
+        movie.setMovieUrl("zxc" + count);
+        movie.setMovieName("zxc" + count);
+        movie.setCreateDate(LocalDateTime.now());
+        movie.setMovieDesc("zxcxcad" + count);
+        movie.setKeepCunt(100);
+        movie.setPhoto("ldhh" + count);
+        movie.setMovieStatus(MovieStatus.ON_LINE_AND_LINK);
+        movie.setShowDate(LocalDate.now());
+        return movie;
     }
 }
